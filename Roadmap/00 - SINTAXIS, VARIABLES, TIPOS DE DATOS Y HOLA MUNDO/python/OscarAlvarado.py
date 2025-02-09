6b3cee783afca92f8e25018e568c00fb6da6c8e9
OscarF2834 <pipesegoviaalvarado@gmail.com>
2025-02-09 18:26:14 -0500

    #01-Python Oscar

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/OscarAlvarado.py
@@ -0,0 +1,29 @@
+#Ejercicio de declaracion de todos los tipos de comentarios y sintaxis
+
+#Comentario con el sitio web oficial del lenguaje de programación
+#https://www.python.org/
+
+#Este es el comentario de una linea
+"""
+Comentario de varias lineas en python
+"""
+
+#Las variables tiene una sintaxis que es:
+variable = "Hola Mundo"
+nombre = "Oscar"
+
+#Como tal en python no existen las constantes, sin embargo se pueden nombrar con letras mayusculas:
+PI = 3,141591
+EULER = 2,719876
+
+"""
+Las variables que existen en python son las siguientes:
+"""
+String = "Hola"
+Integer = 101
+Float = 6.7
+Boolean = True,False
+
+#Impresion de por terminal 
+print("Hola,[Estoy programando en python :)]")
+print(f"Estoy en python")
